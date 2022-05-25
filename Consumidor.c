ea579ab565c39d8c6696bed5b735d84aaa0a5716
zekur <33559356+zekur@users.noreply.github.com>
2022-05-25 15:47:52 -0300

    Add files via upload

---
+++ Consumidor.c
@@ -0,0 +1,136 @@
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define NUMERO 1010
+#define ARCHIVO "/bin/ls"
+#define tam_medio_buffer 3
+#define FlagB1 -1
+#define FlagB2 -2
+#define FlagEOF -3
+
+typedef struct 
+{
+	unsigned int ID;
+	float Tiempo;
+	double Dato;
+}memcomp;
+
+typedef struct 
+{
+	long Id_Aviso;
+	int flag;
+}Aviso;
+
+int main( int argc, char **argv)
+{
+	key_t Clave;
+	int Id_Memoria;
+	memcomp *Memoria = NULL;
+
+	int Id_Cola_Mensajes;
+	Aviso Aviso_fill,Aviso_read;
+	Aviso_read.Id_Aviso = 2;
+
+	int k;
+
+	FILE *Fichero;
+
+	    Clave = ftok (ARCHIVO, NUMERO);
+		if (Clave == -1)
+		{
+			printf("No consegui  clave para memoria compartida\n");
+			exit(1);
+		}
+		
+		//Solicito al sistema operativo la memoria compartida.
+		Id_Memoria = shmget (Clave, 2*tam_medio_buffer*sizeof(memcomp), 0666 | IPC_CREAT);
+		if (Id_Memoria == -1)
+		{
+			printf("No consegui Id para memoria compartida\n");
+			exit (2);
+		}
+
+		//Asocio el ID de memoria a una variable local
+		Memoria = (memcomp *)shmat (Id_Memoria, (const void *)NULL, 0);
+	    
+		if (Memoria == NULL)
+		{
+			printf("No consegui asociar la memoria compartida a una variable\n");
+			exit (3);
+		}
+
+		Id_Cola_Mensajes = msgget (Clave, 0666 | IPC_CREAT);
+		if (Id_Cola_Mensajes == -1)
+		{
+			printf("Error al obtener identificador para cola mensajes\n");
+			exit (-1);
+		}
+
+		if((Fichero=fopen("Datos.csv","w"))==NULL)
+		{
+			printf("\n\n ERROR EN LA APERTURA DEL ARCHIVO\n\n");
+			return 0;
+		}
+Aviso_read.flag=FlagB2;
+	do
+	{
+		
+		msgrcv (Id_Cola_Mensajes, (struct msgbuf *)&Aviso_fill,sizeof(int), 1, 0);
+		if(((Aviso_fill.flag == FlagB1)&&(Aviso_read.flag == FlagB2))||((Aviso_fill.flag != FlagB1) && (Aviso_fill.flag !=FlagB2) && Aviso_fill.flag<tam_medio_buffer))
+		{
+			
+			for(k=0; k<tam_medio_buffer; k++)
+			{
+				printf("Leyendo buffer1\n");
+				fprintf(Fichero, "%u,%f,%lf\n", Memoria[k].ID,Memoria[k].Tiempo,Memoria[k].Dato);
+				printf("id=%u,tiempo=%f,dato=%lf\n", Memoria[k].ID,Memoria[k].Tiempo,Memoria[k].Dato);
+				if(k==Aviso_fill.flag-1)
+				{
+					break;
+				}
+			}
+			if(k==Aviso_fill.flag-1)
+				{
+					Aviso_read.flag=FlagEOF;
+				}
+			else
+				{
+					Aviso_read.flag=FlagB1;
+				}
+			}
+		else if(((Aviso_fill.flag == FlagB2)&&(Aviso_read.flag == FlagB1))||((Aviso_fill.flag != FlagB1) && (Aviso_fill.flag !=FlagB2) && Aviso_fill.flag>=tam_medio_buffer))
+		{
+			
+			for(k=tam_medio_buffer; k<2*tam_medio_buffer; k++)
+			{
+				printf("Leyendo buffer2\n");
+				fprintf(Fichero, "%u,%0.6f,%lf\n", Memoria[k].ID,Memoria[k].Tiempo,Memoria[k].Dato);
+				printf("id=%u,tiempo=%f,dato=%lf\n", Memoria[k].ID,Memoria[k].Tiempo,Memoria[k].Dato);
+				if(k==Aviso_fill.flag-1)
+				{
+					break;
+				}
+			}
+			if(k==Aviso_fill.flag-1)
+				{
+					Aviso_read.flag=FlagEOF;
+				}
+			else
+				{
+					Aviso_read.flag=FlagB2;
+				}
+		}
+		msgsnd (Id_Cola_Mensajes, (struct msgbuf *)&Aviso_read, sizeof(Aviso_read.flag),0);
+	}while(Aviso_read.flag!=FlagEOF);
+
+	shmdt ((const void *) Memoria);
+	shmctl (Id_Memoria, IPC_RMID, (struct shmid_ds *)NULL);
+	//exit(0);
+
+}
---
+++ Productor.c
@@ -0,0 +1,166 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+
+#define NUMERO 1010
+#define ARCHIVO "/bin/ls"
+#define tam_medio_buffer 3
+#define FlagB1 -1
+#define FlagB2 -2
+#define FlagEOF -3
+
+// funcion que permite calcular diferencia de tiempos con precision de
+// microsegundos
+float time_diff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+// estructura que se va a almacenar en la memoria compartida
+typedef struct {
+  unsigned int ID;
+  float Tiempo;
+  double Dato;
+} memcomp;
+
+// estructura del mensaje que se va a usar para avisarle al consumidor que puede
+// leer del buffer 1 o 2
+typedef struct {
+  long Id_Aviso;
+  int flag;
+} Aviso;
+
+Aviso Aviso_fill, Aviso_read;
+
+// estructuras de tiempo para definir un tiempo de inicio y otro de fin para
+// tomar el tiempo que tarda el programa en leer datos
+struct timeval start;
+struct timeval end;
+
+FILE *Fichero;
+// Defino las funciones de llenado del buffer
+void Llenar_Buffer_1(memcomp *Memoria) {
+  int k = 0;
+  do {
+    fscanf(Fichero, "%lf", &Memoria[k].Dato);
+    gettimeofday(&end, NULL);
+    if (feof(Fichero)) {
+      Aviso_fill.flag = k;
+      return;
+    }
+    Memoria[k].ID = k;
+    Memoria[k].Tiempo = time_diff(&start, &end);
+
+    printf("lleno el buffer 1\n");
+    printf("id=%u,tiempo=%f,dato=%lf\n", Memoria[k].ID, Memoria[k].Tiempo,
+           Memoria[k].Dato);
+    k++;
+
+  } while (k < tam_medio_buffer);
+
+  Aviso_fill.flag = FlagB1;
+}
+
+void Llenar_Buffer_2(memcomp *Memoria) {
+  int k = tam_medio_buffer;
+  do {
+    fscanf(Fichero, "%lf", &Memoria[k].Dato);
+    gettimeofday(&end, NULL);
+    if (feof(Fichero)) {
+      Aviso_fill.flag = k;
+      return;
+    }
+    Memoria[k].ID = k;
+    Memoria[k].Tiempo = time_diff(&start, &end);
+
+    printf("lleno el buffer 2\n");
+    printf("id=%u,tiempo=%f,dato=%lf\n", Memoria[k].ID, Memoria[k].Tiempo,
+           Memoria[k].Dato);
+    k++;
+
+  } while (k < 2 * tam_medio_buffer);
+
+  Aviso_fill.flag = FlagB2;
+}
+
+int main(int argc, char **argv) {
+
+  gettimeofday(&start, NULL);
+
+  key_t Clave;
+  int Id_Memoria;
+  memcomp *Memoria = NULL;
+
+  int Id_Cola_Mensajes;
+
+  Aviso_fill.Id_Aviso = 1;
+
+  {
+    if (argc != 2) {
+      printf("\n\n\tModo de uso: Nombre_Programa Nombre_Archivo\n");
+      exit(1);
+    }
+
+    // Consigo la clave usando ARCHIVO y NUMEMER
+
+    Clave = ftok(ARCHIVO, NUMERO);
+    if (Clave == -1) {
+      printf("No consegui  clave para memoria compartida\n");
+      exit(1);
+    }
+
+    // Solicito al sistema operativo la memoria compartida.
+    Id_Memoria =
+        shmget(Clave, 2 * tam_medio_buffer * sizeof(memcomp), 0666 | IPC_CREAT);
+    if (Id_Memoria == -1) {
+      printf("No consegui Id para memoria compartida\n");
+      exit(2);
+    }
+
+    // Asocio el ID de memoria a una variable local
+    Memoria = (memcomp *)shmat(Id_Memoria, (const void *)NULL, 0);
+
+    if (Memoria == NULL) {
+      printf("No consegui asociar la memoria compartida a una variable\n");
+      exit(3);
+    }
+
+    Id_Cola_Mensajes = msgget(Clave, 0600 | IPC_CREAT);
+    if (Id_Cola_Mensajes == -1) {
+      printf("Error al obtener identificador para cola mensajes\n");
+      exit(-1);
+    }
+
+    if ((Fichero = fopen(argv[1], "r")) == NULL) {
+      printf("\n\n ERROR EN LA APERTURA DEL ARCHIVO\n\n");
+      return 0;
+    }
+  }
+  Llenar_Buffer_1(Memoria);
+  msgsnd(Id_Cola_Mensajes, (struct msgbuf *)&Aviso_fill,
+         sizeof(Aviso_fill.flag), 0);
+  Llenar_Buffer_2(Memoria);
+  msgsnd(Id_Cola_Mensajes, (struct msgbuf *)&Aviso_fill,
+         sizeof(Aviso_fill.flag), 0);
+  do {
+    msgrcv(Id_Cola_Mensajes, (struct msgbuf *)&Aviso_read, sizeof(int), 2, 0);
+    if ((Aviso_read.flag == FlagB1) && (Aviso_fill.flag == FlagB2)) {
+      Llenar_Buffer_1(Memoria);
+    } else if ((Aviso_read.flag == FlagB2) && (Aviso_fill.flag == FlagB1)) {
+      Llenar_Buffer_2(Memoria);
+    }
+    msgsnd(Id_Cola_Mensajes, (struct msgbuf *)&Aviso_fill,
+           sizeof(Aviso_fill.flag), 0);
+  } while (Aviso_read.flag != FlagEOF);
+
+  shmdt((const void *)Memoria);
+
+  shmctl(Id_Memoria, IPC_RMID, (struct shmid_ds *)NULL);
+  msgctl(Id_Cola_Mensajes, IPC_RMID, (struct msqid_ds *)NULL);
+
+  exit(0);
+}
